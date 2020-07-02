632752826a703c51551c114bfa3ebe92e300f789
Sebastian <37258153+PenguinDesign@users.noreply.github.com>
2020-07-02 13:43:03 -0500

    Nuevas cosas

--- objects.js
+++ objects.js
@@ -1,5 +1,69 @@
 var list = [
     {
+        link: './assets/pictures/images/showgarden.jpg',
+        description: 'Showgarden'
+    },
+    {
+        link: './assets/pictures/images/showgarden2.jpg',
+        description: 'Showgarden'
+    },
+    {
+        link: './assets/pictures/images/natural.jpg',
+        description: 'Showgarden'
+    },
+    {
+        link: './assets/pictures/images/garden.jpg',
+        description: 'Jardin Key to the Soul'
+    },
+    {
+        link: './assets/pictures/images/garden3.jpg',
+        description: 'Jardin Key to the Soul'
+    },
+    {
+        link: './assets/pictures/images/garden2.jpg',
+        description: 'Jardin Key to the Soul'
+    },
+    {
+        link: './assets/pictures/images/tm2019.jpg',
+        description: 'Hospital HS'
+    },
+    {
+        link: './assets/pictures/images/tm20192.jpg',
+        description: 'Hospital HS'
+    },
+    {
+        link: './assets/pictures/images/tm20193.jpg',
+        description: 'Hospital HS'
+    },
+    {
+        link: './assets/pictures/images/jardin.JPG',
+        description: 'Fraccionamiento Milos'
+    },
+    {
+        link: './assets/pictures/images/jardin2.JPG',
+        description: 'Fraccionamiento Milos en progreso'
+    },
+    {
+        link: './assets/pictures/images/jardin3.JPG',
+        description: 'Fraccionamiento Milos'
+    },
+    {
+        link: './assets/pictures/images/jardin4.jpg',
+        description: 'Fraccionamiento Milos'
+    },
+    {
+        link: './assets/pictures/images/madera.jpg',
+        description: 'Muro verde de madera'
+    },
+    {
+        link: './assets/pictures/images/modulo1.JPG',
+        description: 'Modulo de muro verde'
+    },
+    {
+        link: './assets/pictures/images/modulo2.jpg',
+        description: 'Modulo de muro verde'
+    },
+    {
         link: './assets/pictures/images/alegre.jpg',
         description: 'Jardin de casa en Milenio III'
     },
@@ -28,64 +92,12 @@ var list = [
         description: 'Un deparamento'
     },
     {
-        link: './assets/pictures/images/jardin.JPG',
-        description: 'Fraccionamiento Milos'
-    },
-    {
-        link: './assets/pictures/images/jardin2.JPG',
-        description: 'Fraccionamiento Milos en progreso'
-    },
-    {
-        link: './assets/pictures/images/jardin3.JPG',
-        description: 'Fraccionamiento Milos'
-    },
-    {
-        link: './assets/pictures/images/jardin4.jpg',
-        description: 'Fraccionamiento Milos'
-    },
-    {
-        link: './assets/pictures/images/garden.jpg',
-        description: 'Jardin Key to the Soul'
-    },
-    {
-        link: './assets/pictures/images/garden2.jpg',
-        description: 'Jardin Key to the Soul'
-    },
-    {
-        link: './assets/pictures/images/garden3.jpg',
-        description: 'Jardin Key to the Soul'
-    },
-    {
-        link: './assets/pictures/images/madera.jpg',
-        description: 'Muro verde de madera'
-    },
-    {
-        link: './assets/pictures/images/modulo1.JPG',
-        description: 'Modulo de muro verde'
-    },
-    {
-        link: './assets/pictures/images/modulo2.jpg',
-        description: 'Modulo de muro verde'
-    },
-    {
         link: './assets/pictures/images/nippo1.jpg',
         description: 'Casa muestra Nippo'
     },
     {
         link: './assets/pictures/images/nippo2.jpg',
         description: 'Casa muestra Nippo'
-    },
-    {
-        link: './assets/pictures/images/showgarden.jpg',
-        description: 'Showgarden'
-    },
-    {
-        link: './assets/pictures/images/showgarden2.jpg',
-        description: 'Showgarden'
-    },
-    {
-        link: './assets/pictures/images/natural.jpg',
-        description: 'Showgarden'
     },
     {
         link: './assets/pictures/images/sji2.JPG',
@@ -147,18 +159,6 @@ var list = [
         link: './assets/pictures/images/sn1.jpg',
         description: 'Sendas del Naranjo'
     },
-    {
-        link: './assets/pictures/images/tm2019.jpg',
-        description: 'Hospital Tec Milenio'
-    },
-    {
-        link: './assets/pictures/images/tm20192.jpg',
-        description: 'Hospital Tec Milenio'
-    },
-    {
-        link: './assets/pictures/images/tm20193.jpg',
-        description: 'Hospital Tec Milenio'
-    },
 ];
 
 var left = document.getElementsByClassName('left');
@@ -172,6 +172,7 @@ for (let i = 0; i < list.length; i++) {
 
     img.src = list[i].link;
     img.alt = list[i].description;
+    img.loading = 'lazy';
     text.textContent = list[i].description;
 
     div.classList.add('card', 'mb-5');
